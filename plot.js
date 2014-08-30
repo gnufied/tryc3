1423d53128467a07941e65dc414b6d9430693f2f
Hemant Kumar <gethemant@gmail.com>
2014-08-29 21:47:33 -0400

    Trying real charts working now

--- plot.js
+++ plot.js
@@ -1,22 +1,36 @@
 var PlotChart = function() {
   var that = this;
+  this.counter = 0;
+  _.bindAll(this, 'updateChart');
 };
 
 PlotChart.prototype.plotChart = function() {
-  this.chart = c3.generate({
-    bindto: '#chart',
-    data: {
-      columns: [
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]
-    }
-  });
+  var config = {};
+  config.bindto = '#chart';
+  config.data = {};
+  config.data.keys = {"x":"x","value":["data1"]};
+  config.data.json = [];
+  for(i = 0; i < 10; i++) {
+    config.data.json.push({"x": (new Date()).getTime() - i*1000, "data1": _.random(0,500)});
+  }
+  // config.data.json.push({"x": (new Date()).getTime(), "data1": _.random(0,500)});
+  config.axis = {};
+  config.axis.x = {"type":"timeseries","tick":{"format":"%H:%M:%S"}};
+  config.axis.y = {"label":{"text":"Number of items","position":"outer-middle"}};
+  config.data.types={"data1":"line"};
+  this.chart = c3.generate(config);
+  window.setInterval(this.updateChart, 1000);
 };
 
 PlotChart.prototype.updateChart = function() {
-
+  var value = _.random(0, 500);
+  var data = {};
+  data.keys = {"x":"x","value":["data1"]};
+  data.json = [{"x": new Date(), "data1": value}];
+  //data.length = 2;
+  this.chart.flow(data);
 };
+
 
 var charter = new PlotChart();
 charter.plotChart();
